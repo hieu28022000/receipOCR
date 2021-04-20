8f6077fdbda8a5a12be7a075f10a9c8db32e2c4a
hieu28022000 <18520748@gm.uit.edu.vn>
2021-04-20 20:37:09 +0700

    Upload source

---
+++ utils.py
@@ -0,0 +1,239 @@
+import sys
+import os
+import math
+
+import torch
+from torch.autograd import Variable
+
+from PIL import Image, ImageDraw, ImageFont
+
+import cv2
+from skimage import io
+import numpy as np
+import craft_utils
+import imgproc
+
+from collections import OrderedDict
+
+def copyStateDict(state_dict):
+    if list(state_dict.keys())[0].startswith("module"):
+        start_idx = 1
+    else:
+        start_idx = 0
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = ".".join(k.split(".")[start_idx:])
+        new_state_dict[name] = v
+    return new_state_dict
+
+def str2bool(v):
+    return v.lower() in ("yes", "y", "true", "t", "1")
+
+def test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly, refine_net=None, estimate_num_chars=False):
+    # resize
+    img_resized, target_ratio, size_heatmap = imgproc.resize_aspect_ratio(image, square_size=2048, interpolation=cv2.INTER_LINEAR, mag_ratio=1.5)
+    ratio_h = ratio_w = 1 / target_ratio
+
+    # preprocessing
+    x = imgproc.normalizeMeanVariance(img_resized)
+    x = torch.from_numpy(x).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
+    x = Variable(x.unsqueeze(0))                # [c, h, w] to [b, c, h, w]
+    if cuda:
+        x = x.cuda()
+
+    # forward pass
+    with torch.no_grad():
+        y, feature = net(x)
+
+    # make score and link map
+    score_text = y[0,:,:,0].cpu().data.numpy()
+    score_link = y[0,:,:,1].cpu().data.numpy()
+
+    # Post-processing
+    boxes, polys, mapper = craft_utils.getDetBoxes(score_text, score_link, text_threshold, link_threshold, low_text, poly)
+
+    # coordinate adjustment
+    boxes = craft_utils.adjustResultCoordinates(boxes, ratio_w, ratio_h)
+    polys = craft_utils.adjustResultCoordinates(polys, ratio_w, ratio_h)
+    if estimate_num_chars:
+        boxes = list(boxes)
+        polys = list(polys)
+    for k in range(len(polys)):
+        if estimate_num_chars:
+            boxes[k] = (boxes[k], mapper[k])
+        if polys[k] is None: polys[k] = boxes[k]
+
+    # render results (optional)
+    # render_img = score_text.copy()
+    # render_img = np.hstack((render_img, score_link))
+    # ret_score_text = imgproc.cvt2HeatmapImg(render_img)
+
+    # return boxes, polys, ret_score_text
+    return boxes, polys
+
+def get_textbox(net, image, text_threshold, link_threshold, low_text, cuda, poly, refine_net=None, optimal_num_chars=None):
+    result = []
+    estimate_num_chars = optimal_num_chars is not None
+    bboxes, polys = test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly, refine_net, estimate_num_chars)
+
+    if estimate_num_chars:
+        polys = [p for p, _ in sorted(polys, key=lambda x: abs(optimal_num_chars - x[1]))]
+
+    for i, box in enumerate(polys):
+        poly = np.array(box).astype(np.int32).reshape((-1))
+        result.append(poly)
+
+    return result
+
+def group_text_box(polys, slope_ths = 0.1, ycenter_ths = 0.5, height_ths = 0.5, width_ths = 0.5, add_margin = 0.1, sort_output = True):
+    # poly top-left, top-right, low-right, low-left
+    horizontal_list, free_list,combined_list, merged_list = [],[],[],[]
+
+    for poly in polys:
+        slope_up = (poly[3]-poly[1])/np.maximum(10, (poly[2]-poly[0]))
+        slope_down = (poly[5]-poly[7])/np.maximum(10, (poly[4]-poly[6]))
+        if max(abs(slope_up), abs(slope_down)) < slope_ths:
+            x_max = max([poly[0],poly[2],poly[4],poly[6]])
+            x_min = min([poly[0],poly[2],poly[4],poly[6]])
+            y_max = max([poly[1],poly[3],poly[5],poly[7]])
+            y_min = min([poly[1],poly[3],poly[5],poly[7]])
+            horizontal_list.append([x_min, x_max, y_min, y_max, 0.5*(y_min+y_max), y_max-y_min])
+        else:
+            height = np.linalg.norm( [poly[6]-poly[0],poly[7]-poly[1]])
+            margin = int(1.44*add_margin*height)
+
+            theta13 = abs(np.arctan( (poly[1]-poly[5])/np.maximum(10, (poly[0]-poly[4]))))
+            theta24 = abs(np.arctan( (poly[3]-poly[7])/np.maximum(10, (poly[2]-poly[6]))))
+            # do I need to clip minimum, maximum value here?
+            x1 = poly[0] - np.cos(theta13)*margin
+            y1 = poly[1] - np.sin(theta13)*margin
+            x2 = poly[2] + np.cos(theta24)*margin
+            y2 = poly[3] - np.sin(theta24)*margin
+            x3 = poly[4] + np.cos(theta13)*margin
+            y3 = poly[5] + np.sin(theta13)*margin
+            x4 = poly[6] - np.cos(theta24)*margin
+            y4 = poly[7] + np.sin(theta24)*margin
+
+            free_list.append([[x1,y1],[x2,y2],[x3,y3],[x4,y4]])
+    if sort_output:
+        horizontal_list = sorted(horizontal_list, key=lambda item: item[4])
+
+    # combine box
+    new_box = []
+    for poly in horizontal_list:
+
+        if len(new_box) == 0:
+            b_height = [poly[5]]
+            b_ycenter = [poly[4]]
+            new_box.append(poly)
+        else:
+            # comparable height and comparable y_center level up to ths*height
+            if (abs(np.mean(b_height) - poly[5]) < height_ths*np.mean(b_height)) and (abs(np.mean(b_ycenter) - poly[4]) < ycenter_ths*np.mean(b_height)):
+                b_height.append(poly[5])
+                b_ycenter.append(poly[4])
+                new_box.append(poly)
+            else:
+                b_height = [poly[5]]
+                b_ycenter = [poly[4]]
+                combined_list.append(new_box)
+                new_box = [poly]
+    combined_list.append(new_box)
+
+    # merge list use sort again
+    for boxes in combined_list:
+        if len(boxes) == 1: # one box per line
+            box = boxes[0]
+            margin = int(add_margin*box[5])
+            merged_list.append([box[0]-margin,box[1]+margin,box[2]-margin,box[3]+margin])
+        else: # multiple boxes per line
+            boxes = sorted(boxes, key=lambda item: item[0])
+
+            merged_box, new_box = [],[]
+            for box in boxes:
+                if len(new_box) == 0:
+                    x_max = box[1]
+                    new_box.append(box)
+                else:
+                    if abs(box[0]-x_max) < width_ths *(box[3]-box[2]): # merge boxes
+                        x_max = box[1]
+                        new_box.append(box)
+                    else:
+                        x_max = box[1]
+                        merged_box.append(new_box)
+                        new_box = [box]
+            if len(new_box) >0: merged_box.append(new_box)
+
+            for mbox in merged_box:
+                if len(mbox) != 1: # adjacent box in same line
+                    # do I need to add margin here?
+                    x_min = min(mbox, key=lambda x: x[0])[0]
+                    x_max = max(mbox, key=lambda x: x[1])[1]
+                    y_min = min(mbox, key=lambda x: x[2])[2]
+                    y_max = max(mbox, key=lambda x: x[3])[3]
+
+                    margin = int(add_margin*(y_max - y_min))
+
+                    merged_list.append([x_min-margin, x_max+margin, y_min-margin, y_max+margin])
+                else: # non adjacent box in same line
+                    box = mbox[0]
+
+                    margin = int(add_margin*(box[3] - box[2]))
+                    merged_list.append([box[0]-margin,box[1]+margin,box[2]-margin,box[3]+margin])
+    # may need to check if box is really in image
+    return merged_list, free_list
+
+
+def draw_ocr_box_txt(image,
+                     boxes,
+                     txts,
+                     scores=None,
+                     drop_score=0.5,
+                     font_path="./doc/simfang.ttf"):
+    h, w = image.height, image.width
+    img_left = image.copy()
+    img_right = Image.new('RGB', (w, h), (255, 255, 255))
+
+    import random
+
+    random.seed(0)
+    draw_left = ImageDraw.Draw(img_left)
+    draw_right = ImageDraw.Draw(img_right)
+    for idx, (box, txt) in enumerate(zip(boxes, txts)):
+        if scores is not None and scores[idx] < drop_score:
+            continue
+        color = (random.randint(0, 255), random.randint(0, 255),
+                 random.randint(0, 255))
+        draw_left.polygon(box, fill=color)
+        draw_right.polygon(
+            [
+                box[0][0], box[0][1], box[1][0], box[1][1], box[2][0],
+                box[2][1], box[3][0], box[3][1]
+            ],
+            outline=color)
+        box_height = math.sqrt((box[0][0] - box[3][0])**2 + (box[0][1] - box[3][
+            1])**2)
+        box_width = math.sqrt((box[0][0] - box[1][0])**2 + (box[0][1] - box[1][
+            1])**2)
+        
+        _font = ImageFont.truetype(font_path, 10, encoding="utf-8")
+        size_coef = int((box_width * 10) / _font.getmask(txt).getbbox()[2])
+
+        if box_height > 2 * box_width:
+            font_size = max(int(box_width * 0.9), 10)
+            font = ImageFont.truetype(font_path, size_coef, encoding="utf-8")
+            cur_y = box[0][1]
+            for c in txt:
+                char_size = font.getsize(c)
+                draw_right.text(
+                    (box[0][0] + 3, cur_y), c, fill=(0, 0, 0), font=font)
+                cur_y += char_size[1]
+        else:
+            font_size = max(int(box_height * 0.5), 10)
+            font = ImageFont.truetype(font_path, size_coef, encoding="utf-8")
+            draw_right.text(
+                [box[0][0], box[0][1]], txt, fill=(0, 0, 0), font=font)
+    img_left = Image.blend(image, img_left, 0.5)
+    img_show = Image.new('RGB', (w * 2, h), (255, 255, 255))
+    img_show.paste(img_left, (0, 0, w, h))
+    img_show.paste(img_right, (w, 0, w * 2, h))
+    return np.array(img_show)
